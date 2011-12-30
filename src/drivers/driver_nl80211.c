5caf582cc7a23b6865ae8bb05fd5066836520a83
Pontus Fuchs <pontus.fuchs@gmail.com>
2011-12-30 10:19:35 +0100

    Always set RXFILTER 0 and 1
    In ginger these filters where always set but they where removed
    completely in ICS. They need to be active for unicast and bcast
    to work. Make sure they are always set.
    
    Signed-off-by: Pontus Fuchs <pontus.fuchs@gmail.com>

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -7150,6 +7150,7 @@ static int nl80211_set_wowlan_triggers(struct i802_bss *bss, int enable)
 	struct nl_msg *msg, *pats = NULL;
 	struct nlattr *wowtrig, *pat;
 	int i, ret = -1;
+	int filters;
 
 	bss->drv->wowlan_enabled = !!enable;
 
@@ -7177,8 +7178,13 @@ static int nl80211_set_wowlan_triggers(struct i802_bss *bss, int enable)
 			goto nla_put_failure;
 		}
 
+		/* In ginger filter 0 and 1 are always set but in ICS they
+		   were completely removed. Make sure to always set them
+		   otherwise unicast and bcast are dropped */
+		filters = bss->drv->wowlan_triggers |= 3;
+
 		for (i = 0; i < NR_RX_FILTERS; i++) {
-			if (bss->drv->wowlan_triggers & (1 << i)) {
+			if (filters & (1 << i)) {
 				struct rx_filter *rx_filter = &rx_filters[i];
 				int patnr = 1;
 				u8 *pattern = nl80211_rx_filter_get_pattern(rx_filter,bss);
