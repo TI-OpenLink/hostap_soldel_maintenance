d4604341275de4ff08bac5e7c5cead53d910ba22
Danny Shavit <d-shavit@ti.com>
2012-09-11 17:33:22 +0300

    wpa_s: don't reschedule the sched scan on each scan complete event
    wpa_supplicant doesn't distinguish between NL80211_CMD_SCHED_SCAN_RESULTS
    and NL80211_CMD_NEW_SCAN_RESULTS, this causing unnecessary rescheduling
    of the sched scan on each scan complete event.
    
    Reschedule the sched scan (stop + start) only if the scan results are from
    the sched scan.

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -1164,7 +1164,7 @@ static void mlme_event_remain_on_channel(struct wpa_driver_nl80211_data *drv,
 
 
 static void send_scan_event(struct wpa_driver_nl80211_data *drv, int aborted,
-			    struct nlattr *tb[])
+                            int is_sched_scan_res, struct nlattr *tb[])
 {
 	union wpa_event_data event;
 	struct nlattr *nl;
@@ -1200,6 +1200,9 @@ static void send_scan_event(struct wpa_driver_nl80211_data *drv, int aborted,
 		info->freqs = freqs;
 		info->num_freqs = num_freqs;
 	}
+
+    info->is_sched_scan_res = is_sched_scan_res;
+
 	wpa_supplicant_event(drv->ctx, EVENT_SCAN_RESULTS, &event);
 }
 
@@ -1538,11 +1541,11 @@ static int process_event(struct nl_msg *msg, void *arg)
 		drv->scan_complete_events = 1;
 		eloop_cancel_timeout(wpa_driver_nl80211_scan_timeout, drv,
 				     drv->ctx);
-		send_scan_event(drv, 0, tb);
+		send_scan_event(drv, 0, 0, tb);
 		break;
 	case NL80211_CMD_SCHED_SCAN_RESULTS:
 		wpa_printf(MSG_DEBUG, "nl80211: New sched scan results available");
-		send_scan_event(drv, 0, tb);
+		send_scan_event(drv, 0, 1, tb);
 		break;
 	case NL80211_CMD_SCAN_ABORTED:
 		wpa_printf(MSG_DEBUG, "nl80211: Scan aborted");
@@ -1552,7 +1555,7 @@ static int process_event(struct nl_msg *msg, void *arg)
 		 */
 		eloop_cancel_timeout(wpa_driver_nl80211_scan_timeout, drv,
 				     drv->ctx);
-		send_scan_event(drv, 1, tb);
+		send_scan_event(drv, 1, 0, tb);
 		break;
 	case NL80211_CMD_AUTHENTICATE:
 	case NL80211_CMD_ASSOCIATE:
